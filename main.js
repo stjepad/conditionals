4de409b25079721594a9c8b130b4fdac0551903b
Dejan Stjepanovic <dejanstjepanovic@Dejans-MacBook-Pro.local>
2018-04-03 13:46:06 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+                // Exercise: Using Javascript, create a variable to hold your last name. Using the if-else statement, do the following: If your last name has 7 or more letters, console log "I can't pronounce this last name". Else, console log your last name.
+                // Challenge: Create a new repository on Github and push the code you wrote up to Github.
+
+                const lastName = "Stjepanovic";
+                     if (lastName.length >= 7) {
+                         console.log("I cant pronounce that last name");
+
+                     }
+                     else {
+                         console.log(latName); Stjepanovic
+                     }
